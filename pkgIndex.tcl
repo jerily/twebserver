2d396d1fbed84c0975eaecb4cb7ff188163e96a0
Neofytos Dimitriou <neo@jerily.cy>
2023-08-29 05:42:59 -0400

    renamed module, package, and namespace to avoid conflict with existing package

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,3 +1,3 @@
 set dir [file dirname [info script]]
 
-package ifneeded twebserver 0.1 [list load [file join $dir libtws.so]]
+package ifneeded twebserver 0.1 [list load [file join $dir libtwebserver.so]]
