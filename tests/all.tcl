f9e728a464f15b80d20dc036b71fd6a36607a577
Neofytos Dimitriou <neo@jerily.cy>
2023-09-23 09:57:43 -0400

    initial import of tests

---
+++ tests/all.tcl
@@ -0,0 +1,14 @@
+package require tcltest
+namespace import -force ::tcltest::test
+
+if { [llength $argv] == 0 } {
+    puts stderr "Usage: $argv0 libdir"
+    exit 1
+}
+
+set auto_path [linsert $auto_path 0 [lindex $argv 0]]
+set argv [lrange $argv 1 end]
+
+::tcltest::configure -testdir [file dirname [info script]]
+
+exit [::tcltest::runAllTests]
---
+++ tests/setup_server_echo.tcl
@@ -0,0 +1,37 @@
+package require twebserver
+
+set server_port 12345
+
+proc process_request {request_dict} {
+    return "HTTP/1.1 200 OK\n\ntest message request_dict=$request_dict\n"
+}
+
+proc process_conn {conn addr port} {
+    #puts "process_conn $conn $addr $port"
+    if { [catch {
+        set request [::twebserver::read_conn $conn]
+        ::twebserver::write_conn $conn $request
+    } errmsg] } {
+        puts "error: $errmsg"
+    }
+    ::twebserver::close_conn $conn
+}
+
+set dir [file dirname [info script]]
+set config_dict [dict create]
+set server_handle [::twebserver::create_server $config_dict process_conn]
+::twebserver::add_context $server_handle localhost "${dir}/../certs/host1/key.pem" "${dir}/../certs/host1/cert.pem"
+#after 100 [list puts "event loop works fine"]
+::twebserver::listen_server $server_handle $server_port
+
+proc accept {chan addr port} {
+    global forever
+    set forever 1
+    close $chan
+}
+
+set sock [socket -server accept 11111]
+
+vwait forever
+::twebserver::destroy_server $server_handle
+close $sock
