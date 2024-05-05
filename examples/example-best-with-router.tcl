0252859bc2acd10dc0bbc02871e73e4731e38207
Neofytos Dimitriou <neo@jerily.cy>
2024-05-05 16:12:10 -0400

    initial import of signal handler for graceful shutdown (11) - reverted change in example

--- examples/example-best-with-router.tcl
+++ examples/example-best-with-router.tcl
@@ -81,8 +81,6 @@ set init_script {
     }
 
     proc post_example_handler {ctx req} {
-        return [::twebserver::build_response 200 text/plain \
-            "test message POST addr=[dict get $ctx addr] headers=[dict get $req headers]"]
 
         set form [::twebserver::get_form $req]
         #puts form=$form
