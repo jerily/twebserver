0295ccc10db2a2e99b4832a3f117c6031a7d6ff3
Neofytos Dimitriou <neo@jerily.cy>
2024-08-18 03:22:36 -0400

    github workflows: added tests

--- tests/all.tcl
+++ tests/all.tcl
@@ -1,14 +1,6 @@
 package require tcltest
 namespace import -force ::tcltest::test
 
-if { [llength $argv] == 0 } {
-    puts stderr "Usage: $argv0 libdir"
-    exit 1
-}
-
-set auto_path [linsert $auto_path 0 [lindex $argv 0]]
-set argv [lrange $argv 1 end]
-
-::tcltest::configure -singleproc true -testdir [file dirname [info script]]
+::tcltest::configure {*}$argv -singleproc true -testdir [file dirname [info script]]
 
 exit [::tcltest::runAllTests]
